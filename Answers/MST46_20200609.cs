6b1115a2e9d33c372ebd586328db5290ef77149e
wanghongqing <2437695972@qq.com>
2020-06-09 16:57:21 +0800

    临时提交

---
+++ Answers/MST46_20200609.cs
@@ -0,0 +1,34 @@
+namespace LeetCodeExercise.Answers
+{
+    /*
+https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof/
+面试题46. 把数字翻译成字符串
+给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+
+示例 1:
+
+输入: 12258
+输出: 5
+解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+ 
+提示：
+
+0 <= num < 2^31 = 2147483648
+     */
+    
+    public class MST46_20200609
+    {
+        //最先想到的是迈台阶的问题
+        //所以想要用递归的方式解决
+        public int TranslateNum(int num)
+        {
+
+            return -1;
+
+        }
+        
+        //题解思路
+        
+        //其他思路
+    }
+}
